1c53a40a40593d470f7be298ffb3d3ee840e78d3
NirZzZz <168575535+NirZzZz@users.noreply.github.com>
2024-09-25 09:47:34 +0300

    frontend commit

---
+++ web_app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from db_connector import get_user
+
+app = Flask(__name__)
+
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user_name(user_id):
+    try:
+        data = get_user(user_id)
+        user_name = data[0][1]
+        if user_name:
+            return (f"""
+                    <html>
+                    <body>
+                        <h1 id='user'>{user_name}</h1>
+                    </body>
+                    </html>
+                    """)
+        else:
+            return (f"""
+                    <html>
+                    <body>
+                        <h1 id='error'>No such user: {user_id}</h1>
+                    </body>
+                    </html>
+                    """)
+    except IndexError as e:
+        return (f"""
+                <html>
+                <body>
+                    <h1 id='error'>No such user: {user_id}</h1>
+                </body>
+                </html>
+                """)
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5001, debug=True)
