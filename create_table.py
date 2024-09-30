2831c3d19a74bd5cd0b525fd13cb70c7dc77f891
NirZzZz <168575535+NirZzZz@users.noreply.github.com>
2024-09-30 18:13:12 +0300

    Docker complete commit

--- create_table.py
+++ create_table.py
@@ -12,8 +12,7 @@ def create_connection():
         try:
             print(f"Attempt {attempt + 1} to connect to the database...")
             conn = mysql.connector.connect(
-                host=os.getenv('DATABASE_HOST', 'db'),
-                port=int(os.getenv('DATABASE_PORT', 3306)),
+                port=int(os.getenv('DATABASE_PORT', 3307)),
                 user=os.getenv('DATABASE_USER', 'root'),
                 password=os.getenv('DATABASE_PASSWORD', 'adminadmin')
             )
